067d83fcf90670ed5303b6a3c64836228c1ae2fa
minyoungci <dbssus123@gmail.com>
2023-02-12 19:08:50 +0900

    Sibal

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Text } from "@chakra-ui/react";
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <Text color={"red.600"} fontSize={"6xl"}>
+        working   
+      </Text>
+    </div>
+  );
+}
+
+
+export default App;

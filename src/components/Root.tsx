0facf6c5c7f26c5081c2686a842c057e32ca28a6
minyoungci <dbssus123@gmail.com>
2023-02-13 14:59:02 +0900

    Chakra UI - Header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,25 @@
+import { Box, HStack, Button } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom"
+import { FaAirbnb } from "react-icons/fa"
 
 export default function Root() {
     return (
-        <h1>
-            im root
+        <Box>
+            <HStack 
+                justifyContent={"space-between"}
+                py={5} 
+                px={10} 
+                borderBottomWidth={1}
+            >
+                <Box color="red.500">
+                    <FaAirbnb size={"48"} />
+                </Box>
+                <HStack spacing={"2"}>
+                    <Button>Log in</Button>
+                    <Button colorScheme={"red"}>Sign up</Button>
+                </HStack>
+            </HStack>
             <Outlet />
-        </h1>
+        </Box>
     );
 }

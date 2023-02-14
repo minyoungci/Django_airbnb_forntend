da6d0b40da5dc5e78b6281ea741d3597504b6e8f
minyoungci <dbssus123@gmail.com>
2023-02-14 13:56:34 +0900

    Dark mode - Chakra UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
-import { FaAirbnb, FaMoon } from "react-icons/fa";
+import { FaAirbnb, FaMoon, FaSun} from "react-icons/fa";
 import {
   Box,
   Button,
   HStack,
   IconButton,
+  useColorMode,
   useDisclosure,
+  LightMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
@@ -21,6 +24,9 @@ export default function Header() {
     onClose: onSignUpClose,
     onOpen: onSignUpOpen,
   } = useDisclosure();
+  const{ colorMode, toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue("red.500", "red.200"); // useColorModeValue(라이트 모드일 때, 다크모드일 때)
+  const Icon = useColorModeValue(FaMoon, FaSun); // components 에 적용할 때는 첫 글자가 무조건 대문자가 와야함.
   return (
     <HStack
       justifyContent={"space-between"}
@@ -28,21 +34,24 @@ export default function Header() {
       px={10}
       borderBottomWidth={1}
     >
-      <Box color="red.500">
+      <Box color={logoColor}>
         <Link to={"/"}>
           <FaAirbnb size={"48"} />
         </Link>
       </Box>
       <HStack spacing={2}>
         <IconButton
+          onClick={toggleColorMode}
           variant={"ghost"}
           aria-label="Toggle dark mode"
-          icon={<FaMoon />}
+          icon={<Icon />}
         />
         <Button onClick={onLoginOpen}>Log in</Button>
-        <Button onClick={onSignUpOpen} colorScheme={"red"}>
-          Sign up
-        </Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme={"red"}>
+            Sign up
+          </Button>
+        </LightMode>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
